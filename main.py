a9caa555487e27aed14c21c6cd1d144f012fd57e
Billy <billy.luong7@gmail.com>
2021-05-04 15:40:22 -0400

    fixed an error

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import os
 import requests
 import json
+import random
 
 # Bot from youtube tutorial
 def get_quote():
@@ -19,7 +20,7 @@ starter_encouragements = ["Cheer up!"]
 # events for the bot to work
 @client.event
 async def on_ready():
-    print("We have logged in as {0.user}.format(client)")   # 0 becomes client and user is how you get the username
+    print("We have logged in as {0.user}".format(client))   # 0 becomes client and user is how you get the username
 
 @client.event
 async def on_message(message):
@@ -32,6 +33,6 @@ async def on_message(message):
         await message.channel.send(quote)
 
     if any(word in msg for word in sad_words):
-        await message.send(starter_encouragements[0])
+        await message.channel.send(random.choice(starter_encouragements))
 
 client.run(os.getenv("TOKEN"))
