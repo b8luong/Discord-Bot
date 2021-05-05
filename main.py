699e115805423de398b0ae209b0eda754e20c94b
Billy <billy.luong7@gmail.com>
2021-05-05 16:18:11 -0400

    added flask and mysql

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
+from dotenv import load_dotenv
+from flask import Flask
 import discord
 import os
 import requests
 import json
+import mysql.connector
 import random
+
+# .env file from root
+
+load_dotenv()
+
+# initializing variables from env file
+TOKEN = os.getenv("TOKEN")
+app_id = os.getenv("APP_ID")
+public_key = os.getenv("PUBLIC_KEY")
+# mySql credentials
+sqlHost = os.getenv("host")
+sqlUser = os.getenv("user")
+sqlPasswd = os.getenv("passwd")
+
+# Creating flask app.
+app = Flask(__name__)
+
+# initializing mysql db
+db = mysql.connector.connect(
+    host=sqlHost,
+    user=sqlUser,
+    passwd=sqlPasswd
+)
+
+mycursor = db.cursor()
+mycursor.execute("CREATE DATABASE reminders")
 
 # Bot from youtube tutorial
 def get_quote():
@@ -35,4 +64,20 @@ async def on_message(message):
     if any(word in msg for word in sad_words):
         await message.channel.send(random.choice(starter_encouragements))
 
+    # example to do split
+    '''
+    if msg.startswith("$new"):
+        encouraging_message = msg.split("$new ",1)[1]
+        update_encouragements(encouraging_message)
+        await message.channel.send("New encouraging message added.")
+    '''
+
+    if msg.startswith("!setdate"):
+        reminder = msg.split("!setdate ",1)[1]
+        await message.channel.send("Reminder has been set")
+
 client.run(os.getenv("TOKEN"))
+
+# start app
+if __name__ == "__main__":
+    app.run(debug=True)
