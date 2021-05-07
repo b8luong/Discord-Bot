b7129678cf74b07c707966994a8312ae84af72b0
Billy <billy.luong7@gmail.com>
2021-05-07 16:37:19 -0400

    Created table to store the parsed message content

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import os
 import requests
 import json
 import mysql.connector
-import random
+import time
+from datetime import datetime
 
 # error logging
-logging.basicConfig(level=logging.info)
+logging.basicConfig(level=logging.INFO)
 
 # .env file
 load_dotenv()
@@ -22,7 +23,7 @@ public_key = os.getenv("PUBLIC_KEY")
 sqlHost = os.getenv("host")
 sqlUser = os.getenv("user")
 sqlPasswd = os.getenv("passwd")
-
+sqlDatabase = os.getenv("database")
 # Creating flask app.
 app = Flask(__name__)
 
@@ -30,11 +31,14 @@ app = Flask(__name__)
 db = mysql.connector.connect(
     host=sqlHost,
     user=sqlUser,
-    passwd=sqlPasswd
+    passwd=sqlPasswd,
+    database=sqlDatabase
 )
 
 mycursor = db.cursor()
 # mycursor.execute("CREATE DATABASE reminders")
+# mycursor.execute("CREATE TABLE reminders (id INT AUTO_INCREMENT PRIMARY KEY, date VARCHAR(255), who VARCHAR(255), what VARCHAR(255), remind VARCHAR(255))")
+
 
 # Bot from youtube tutorial
 def get_quote():
@@ -57,6 +61,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     msg = message.content
+
     if message.author == client.user:
         return
 
@@ -64,8 +69,8 @@ async def on_message(message):
         quote=get_quote()
         await message.channel.send(quote)
 
-    if any(word in msg for word in sad_words):
-        await message.channel.send(random.choice(starter_encouragements))
+    # if any(word in msg for word in sad_words):
+    #     await message.channel.send(random.choice(starter_encouragements))
 
     # example to do split
     '''
@@ -76,10 +81,29 @@ async def on_message(message):
     '''
 
     if msg.startswith("!setdate"):
-        reminder = msg.split("!setdate ",1)[1]
-        print(reminder)
-        await message.channel.send("Reminder has been set")
+        # split the message on the command word
+        content = msg.split("!setdate",1)[1]
+        print(content)
+        list = content.split("\n")
+        print(list)
 
+        # store the time of the event
+        when_str = list[1].split("When: ",1)[1]
+        date_obj = datetime.strptime(when_str, "%m/%d/%y %H:%M:%S")
+        print(date_obj)
+        # store the people to mention for the reminder
+        who = list[2].split("Who: ",1)[1]
+        print(who)
+        # store the message to remind
+        what = list[3].split("What: ",1)[1]
+        print(what)
+        # store into table for the future
+        mycursor.execute("INSERT INTO reminders VALUES(NULL, %s, %s, %s, 'fill')",(when_str,who,what))
+
+        # grab the user id for who set the reminder
+        user = message.author.id
+        # await message.channel.send(f"<@{user}> Reminder has been set. \n{content} ")
+        await message.channel.send(f"<@{user}>\nWhen: {when_str}\nWho: {who}\nWhat: {what}\n")
 client.run(os.getenv("TOKEN"))
 
 # start app
